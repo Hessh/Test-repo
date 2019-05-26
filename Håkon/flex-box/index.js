1d599424348a8d299172cd392a37d1f13bb6463c
Hessh <haakon.hess@gmail.com>
2019-05-26 19:49:41 +0200

    Add grid-view

---
+++ Håkon/flex-box/index.js
@@ -0,0 +1,14 @@
+
+
+let containerElement = document.getElementsByClassName("container");
+let plusElement = document.getElementsByClassName("plusButton");
+
+plusElement.addEventListener("click", function(e){
+
+    console.log()
+    if(e.target.tagName = ""){
+
+    }
+});
+
+console.log("hei");
