999d580a7360c74dbd00694e7962e3e38883e809
Richard Greger Bjørn <richardgreger@techmekk.no>
2019-05-29 13:06:53 +0200

    Added .js that can append Childs into the main columns

---
+++ Richard/Test grid-view and flex-box/29.05/gird-view/index.js
@@ -0,0 +1,16 @@
+var newItem2 = document.getElementsByClassName("column-2");
+var i = 1;
+function myFunction()
+{
+    var newItem = document.createElement("div");
+    newItem.innerHTML = '<div class="item"><div class="content">'+
+    '<h4>' + i + '</h4><h5>Name</h5><p>Innhold</p></div>'+
+    '<div class="progress"><div class="button">Trykk</div>'+
+    '<div class="button">nr</div>'+
+    '<div class="button"></div>'+
+    '</div></div>';
+    document.getElementsByClassName("column-4")[0].appendChild(newItem);
+    ++i;
+
+
+};
