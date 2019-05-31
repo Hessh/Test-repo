019f8200ef9bd80bee381d2a689799a5bf22db0f
Hessh <haakon.hess@gmail.com>
2019-05-31 13:52:16 +0200

    Updated filters-files

--- Håkon/filters/index.js
+++ Håkon/filters/index.js
@@ -1,20 +1,45 @@
-function select(source){
-    const checkboxes = document.getElementsByName("show-task");
+function slect(selector){
+    
+    const checkboxes = document.getElementsByClassName("show-task");
+    
+    console.log(checkboxes)
+    if(selector.checked == true){
+        
+        for(let i = 0; i < checkboxes.length; i++){
+            checkboxes[i].checked = true;
+            
+        }
+        console.log("Truls");
+    }else{
+        for(let i = 0; i < checkboxes.length; i++){
+            checkboxes[i].checked = false;
+        }
+        console.log("heisann");
+    }  
+}
 
-    for(let checkbox in checkboxes){
-        checkbox.checked = source.checked;
-        console.log("hei");
+function myFunction(colaborator){
+    let checkBox = document.getElementById(colaborator);
+
+    let tasks = document.getElementsByClassName("task");
+
+    for (let i = 0; i < tasks.length; i++){
+    
+    let tasks2 = tasks[i].children;
+    let tasks3 = tasks2[0].children;
+    let tasks4 = tasks3[1].innerHTML;
+    console.log(tasks4);
+    console.log(checkBox.value);
+
+        if(tasks4.toLowerCase() == (checkBox.value).toLowerCase()){
+
+            if(checkBox.checked == false){
+                tasks[i].style.display = "none";
+        
+            }else{
+                tasks[i].style.display = "block";
+            }
+        }
     }
 }
 
-function myFunction(){
-    let checkBox = document.getElementById("show-task");
-
-    let kak = document.getElementById("kak");
-
-    if(checkBox.checked == false){
-        kaka.style.display = "none";
-    }else if(checkBox.checked == true){
-        kak.style.display = "block";
-    }
-}
